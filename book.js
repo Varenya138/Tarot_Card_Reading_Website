abf744fb69c1bf7b44d81103458bb2064dffb428
Varenya Sharma <100291600+Varenya138@users.noreply.github.com>
2024-08-28 20:39:33 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,7 @@
+<!-- Include in `services.html` -->
+<script>
+document.querySelector("form").addEventListener("submit", function(event) {
+    Event.preventDefault();
+    alert("Your tarot reading has been booked!");
+});
+</script>
